c3ab8d6248ec1645d0786bf2ee9e7b99f0ac9f5c
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2019-07-06 03:43:44 +0700

    implementasi sender apn & fcm

--- buku/bab4/codes/pengiriman_packet_ke_apns.java
+++ buku/bab4/codes/pengiriman_packet_ke_apns.java
@@ -1,1 +1,91 @@
-// TODO
+@Component
+public class PacketConsumer {
+    ...
+    @KafkaListener(topics = PlatformType.IOS_TOPIC)
+    public void consume(ConsumerRecord<String, Packet> record) {
+        LOG.info("consume record {}.", record.key());
+        var packet = record.value();
+        try {
+            var client = packet.getBatch().getClientDestination();
+            apnService.sendPacket(client, packet);
+        } catch (Exception e) {
+            packetService.updateSent(packet, PacketStatus.FAILED, e.toString());
+        }
+    }
+}
+
+@Service
+public class ApnService {
+    ...
+    public Packet sendPacket(Client client, Packet packet) throws ExecutionException, InterruptedException, ApnsClientException, IOException, SQLException {
+        LOG.info("send packet {}.", packet.getId());
+        var internalClient = clientService.getInternalClient(client);
+        var notification = createNotification(packet);
+        var response = internalClient.sendNotification(notification).get();
+        if (!response.isAccepted()) throw new ApnsClientException(response.getRejectionReason());
+        return packetService.updateSent(packet, PacketStatus.SUCCESS, null);
+    }
+
+    private ApnsPushNotification createNotification(Packet packet) {
+        var token = packet.getDevice().getToken();
+        var client = packet.getBatch().getClientDestination();
+        var certificate = certificateService.findApnCertificate(client);
+        var bundleId = certificate.getBundleId();
+        var batch = packet.getBatch();
+        var payload = createPayload(batch);
+        return new SimpleApnsPushNotification(token, bundleId, payload);
+    }
+
+    private String createPayload(Batch batch) {
+        var builder = new ApnsPayloadBuilder();
+        builder.setAlertTitle(batch.getTitle());
+        builder.setAlertBody(batch.getBody());
+        if (batch.getImage() != null) builder.addCustomProperty("image", batch.getImage());
+        if (batch.getSound() != null) builder.addCustomProperty("sound", batch.getSound());
+        if (batch.getAction() != null) builder.addCustomProperty("action", batch.getAction());
+        if (batch.getAdditionalData() != null) builder.addCustomProperty("additional_data", batch.getAdditionalData());
+        return builder.buildWithDefaultMaximumLength();
+    }
+}
+
+@Service
+public class ApnClientService {
+    ...
+    @Cacheable(cacheNames = "ApnsClient", key = "#client.id.toString()", sync = true)
+    public ApnsClient getInternalClient(Client client) throws SQLException, IOException {
+        LOG.info("initialize apns-client for client {}.", client.getId());
+        var certificate = certificateService.findApnCertificate(client);
+        var apnsClient = new ApnsClientBuilder()
+                .setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST)
+                .setClientCredentials(certificate.getKeyStream(), certificate.getPassword())
+                .setConcurrentConnections(8)
+                .build();
+        executor.schedule(apnsClient::close, 30, TimeUnit.MINUTES);
+        return apnsClient;
+    }
+}
+
+@Service
+public class CertificateService {
+    ...
+    public Certificate findApnCertificate(Client client) {
+        return repository
+                .findByClientAndType(client, SenderType.APN)
+                .orElseThrow(() -> APN_CERTIFICATE_NOT_FOUND);
+    }
+    ...
+}
+
+@Service
+public class PacketService {
+    ...
+    @Transactional
+    public Packet updateSent(Packet packet, PacketStatus status, String reason) {
+        packet.setStatus(status);
+        packet.setReason(reason);
+        packet.setSentAt(LocalDateTime.now());
+        return packetRepository.save(packet);
+    }
+    ...
+}
+
--- buku/bab4/codes/pengiriman_packet_ke_fcm.java
+++ buku/bab4/codes/pengiriman_packet_ke_fcm.java
@@ -1,1 +1,83 @@
-// TODO
+@Component
+public class PacketConsumer {
+    ...
+    @KafkaListener(topics = {PlatformType.ANDROID_TOPIC, PlatformType.WEB_TOPIC})
+    public void consume(ConsumerRecord<String, Packet> record) {
+        LOG.info("consume record {}.", record.key());
+        var packet = record.value();
+        try {
+            fcmService.sendPacket(packet.getBatch().getClientDestination(), packet);
+        } catch (Exception e) {
+            packetService.updateSent(packet, PacketStatus.FAILED, e.toString());
+        }
+    }
+}
+
+@Service
+public class FcmService {
+    ...
+    public Packet sendPacket(Client client, Packet packet) throws FirebaseMessagingException, IOException, SQLException {
+        LOG.info("send packet {}.", packet.getId());
+        var internalClient = clientService.getInternalClient(client);
+        var message = createMessage(packet);
+        internalClient.send(message);
+        return packetService.updateSent(packet, PacketStatus.SUCCESS, null);
+    }
+
+    private Message createMessage(Packet packet) {
+        var batch = packet.getBatch();
+        var device = packet.getDevice();
+        var builder = Message.builder();
+        builder.setToken(device.getToken());
+        builder.setNotification(new Notification(batch.getTitle(), batch.getBody()));
+        if (batch.getImage() != null) builder.putData("image", batch.getImage());
+        if (batch.getSound() != null) builder.putData("sound", batch.getSound());
+        if (batch.getAction() != null) builder.putData("action", batch.getAction());
+        if (batch.getAdditionalData() != null) builder.putData("additional_data", batch.getAdditionalData());
+        return builder.build();
+    }
+}
+
+@Service
+public class FcmClientService {
+    ...
+    @Cacheable(cacheNames = "FirebaseMessaging", key = "#client.id.toString()", sync = true)
+    public FirebaseMessaging getInternalClient(Client client) throws IOException, SQLException {
+        LOG.info("initialize fcm-client for client {}.", client.getId());
+        var certificate = certificateService.findFcmCertificate(client);
+        var keyStream = certificate.getKeyStream();
+        var googleCredentials = GoogleCredentials.fromStream(keyStream);
+        var threadManager = new ConcurrentThreadManager(8);
+        var options = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
+                .setThreadManager(threadManager)
+                .build();
+        var firebaseApp = FirebaseApp.initializeApp(options, UUID.randomUUID().toString());
+        executor.schedule(firebaseApp::delete, 30, TimeUnit.MINUTES);
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
+
+@Service
+public class CertificateService {
+    ...
+    public Certificate findFcmCertificate(Client client) {
+        return repository
+                .findByClientAndType(client, SenderType.FCM)
+                .orElseThrow(() -> FCM_CERTIFICATE_NOT_FOUND);
+    }
+}
+
+@Service
+public class PacketService {
+    ...
+    @Transactional
+    public Packet updateSent(Packet packet, PacketStatus status, String reason) {
+        packet.setStatus(status);
+        packet.setReason(reason);
+        packet.setSentAt(LocalDateTime.now());
+        return packetRepository.save(packet);
+    }
+    ...
+}
+
